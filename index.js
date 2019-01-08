a57b285a7ae4b91b2ac4f8737e5b6d3ec50e9866
Scout Quinn <imogen@dfdt.me>
2019-01-08 13:47:57 +1100

    data upload test

--- index.js
+++ index.js
@@ -1266,7 +1266,7 @@ function showResults(values) {
 		type: 'POST',
 		contentType: 'application/json',
 		dataType: 'json',
-		data: JSON.stringify({"q_1": values["q_1"}),
+		data: JSON.stringify({"q_1": values["q_1"]}),
 		success: function success(result) {
 			console.log(result);
 		},
